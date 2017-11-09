9cf9bad25c3b785e8858f2276705984434d9155e
Marjorie Ivy <morenomt27@gmail.com>
2017-11-09 10:47:57 -0600

    Committing 10 days of JS and 2 Days of Coding

---
+++ 30Days/Day3Conditional/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        /*Task 
+Given an integer, N, perform the following conditional actions:
+
+If N is odd, print Weird
+If N is even and in the inclusive range of  to , print Not Weird
+If N is even and in the inclusive range of  to , print Weird
+If N is even and greater than , print Not Weird
+         * 
+         * */
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number: ");
+            int N = Convert.ToInt32(Console.ReadLine());
+
+            if (N % 2 == 1 || (N > 5 && N <= 20))
+            {
+
+                Console.WriteLine("Weird - Odd Number");
+            }
+            else
+            {
+                Console.WriteLine("Not Weird - Even Number");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Factorial/Factorial/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factorial
+{
+    class Program
+    {
+        // Factorialize a number
+        // 5! = 5 * 4 * 3 * 2 * 1 = 120(factorial of 5)
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number to factorialize: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            int fact = factorialize(num);
+            Console.WriteLine("Factorial of number entered is " + fact);
+            Console.ReadKey();
+        }
+
+        static int factorialize(int num)
+        {
+                        
+            if(num<0)
+            {
+                return -1;
+            }
+            else if(num.Equals(0))
+            {
+                return 1;
+            }
+            else
+            {
+              return (num * factorialize(num - 1));
+            }
+            
+        }
+    }
+}
