0af002590519be0b755d404c8b2023614a832c78
Marjorie Ivy <morenomt27@gmail.com>
2017-11-10 10:21:58 -0600

    Committing Day 2 of coding challenge

---
+++ 30Days/Day2Operators/Day2Operators/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day2Operators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {        
+            Console.WriteLine("Enter meal cost: ");
+            double mealCost = double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter tip Percent: ");
+            double tipPercent = double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter tax Percent: ");
+            double taxPercent = double.Parse(Console.ReadLine());
+            Cost(mealCost, tipPercent, taxPercent);
+            Console.ReadKey();
+        }
+        static double Cost(double mealCost, double tipPercent, double taxPercent)
+        {
+            double tip;
+            double tax;
+            double totalCost;
+
+            tip = mealCost * (tipPercent / 100.0);
+            tax = mealCost * (taxPercent / 100.0);
+            totalCost = mealCost + tip + tax;
+
+            Console.WriteLine("The total meal cost is {0:F0} dollars.", totalCost);
+            return totalCost;
+        }
+        
+    }
+}
---
+++ Letandconst/Letandconst/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Letandconst
+{
+    class Program
+    {
+        /**print the area of a circle having radius
+         * print the perimeter of a circle having radius**/
+        static void Main(string[] args)
+        {
+            double radius;
+            Console.WriteLine("Enter radius");
+            radius = double.Parse(Console.ReadLine());
+            Circle(radius);
+            Console.ReadLine();
+        }
+
+        static double Circle(double radius)
+        {
+            double area = Math.PI * radius * radius;
+            double perimeter = 2 * Math.PI * radius;
+            Console.WriteLine("Area is {0:F1}", area);
+            Console.WriteLine("Perimeter is {0:F1}", perimeter);
+            return radius;
+        }
+    }
+}
